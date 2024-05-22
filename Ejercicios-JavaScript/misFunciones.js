e740d62cd4fd3cbffce784a1fe42821ead04e72b
Franciscafalco <franciscafalco4@gmail.com>
2024-05-22 10:42:01 -0300

    Se realizó el ejercicio de grados y radianes

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -2,9 +2,10 @@
  * Conversion de unidades, de metros, yardas, pies y pulgadas.
  * @method cambiarUnidades
  * @param {string} nombre - El nombre de los inputs de metros, yardas, pies o pulgadas
- * @param {number} valor - El valor de los inputs de metros, yardas, pies o pulgadas
+ * @param {number} valor - El valor de los inputs de metros, yardas, pies o pulgada
  */
 function convertirUnidades(nombre, valor){
+    //TODO: Debería admitir números con coma.
     if (isNaN(valor)){
         document.getElementById("metro").value="";
         document.getElementById("pie").value="";
@@ -32,4 +33,24 @@ function convertirUnidades(nombre, valor){
         document.getElementById("pulgada").value = valor * 36;
         document.getElementById("pie").value = valor * 3;
     }
+}
+/**
+ * Permite convertir grados a radianes y viceversa.
+ * @method convertirGR
+ * @param {string} id - grados, radianes
+ */
+function convertirGR(id){
+    let grad,rad;
+    //TODO: Agregar mensajes de error cuando se agregan letras.
+    //TODO: Debería admitir números con coma.
+    if (id=="grados") {
+        grad = document.getElementById("grados").value;
+        rad = (grad * Math.PI) / 180;
+        document.getElementById("radianes").value=rad;
+    }
+        else if (id=="radianes"){
+            rad=document.getElementById("radianes").value;
+            grad=(rad*180)/Math.PI
+        document.getElementById("grados").value=grad;
+        }
 }
