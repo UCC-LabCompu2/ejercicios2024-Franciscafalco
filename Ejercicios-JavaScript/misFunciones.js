214e77cb3059acbdbc590b9dc54554daff25e2d7
Franciscafalco <franciscafalco4@gmail.com>
2024-05-29 10:51:22 -0300

    Se identó el código y se realizó ejercicio PASAR VALORES DE PRIMERAWEB A SEGUNDAWEB

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -4,69 +4,65 @@
  * @param {string} nombre - El nombre de los inputs de metros, yardas, pies o pulgadas
  * @param {number} valor - El valor de los inputs de metros, yardas, pies o pulgada
  */
-let convertirUnidades=(nombre, valor)=>{
+let convertirUnidades = (nombre, valor) => {
     //TODO: Debería admitir números con coma.
-    let metro,pulgada,pie,yarda;
+    let metro, pulgada, pie, yarda;
 
-    if(valor.includes(",")){
-        valor=valor.replace(",",".");
+    if (valor.includes(",")) {
+        valor = valor.replace(",", ".");
     }
 
-    if (isNaN(valor)){
-        metro="";
-        pie="";
-        pulgada="";
-        yarda="";
+    if (isNaN(valor)) {
+        metro = "";
+        pie = "";
+        pulgada = "";
+        yarda = "";
         alert("El valor ingresado no es correcto")
-    }
-    else if(nombre==="metro"){
-        metro=valor;
+    } else if (nombre === "metro") {
+        metro = valor;
         pulgada = valor * 39.3701;
         pie = valor * 3.28084;
-        yarda= valor * 1.09361;
-    }
-    else if (nombre==="pulgada"){
-        pulgada=valor;
-        metro = valor * 0.0254 ;
-        pie= valor * 0.833333;
-        yarda= valor * 0.02777778;
-    }
-    else if (nombre==="pie"){
-        pie=valor;
+        yarda = valor * 1.09361;
+    } else if (nombre === "pulgada") {
+        pulgada = valor;
+        metro = valor * 0.0254;
+        pie = valor * 0.833333;
+        yarda = valor * 0.02777778;
+    } else if (nombre === "pie") {
+        pie = valor;
         metro = valor * 0.3048;
         pulgada = valor * 12;
         yarda = valor * 0.333333;
-    }
-    else if (nombre==="yarda"){
-        yarda=valor;
+    } else if (nombre === "yarda") {
+        yarda = valor;
         metro = valor * 0.9144;
         pulgada = valor * 36;
         pie = valor * 3;
     }
-    document.getElementById("metro").value=Math.round(metro*100)/100;
-    document.getElementById("pie").value=Math.round(pie*100)/100;
-    document.getElementById("pulgada").value=pulgada.toFixed(2);
-    document.getElementById("yarda").value=yarda.toFixed(2);
+    document.getElementById("metro").value = Math.round(metro * 100) / 100;
+    document.getElementById("pie").value = Math.round(pie * 100) / 100;
+    document.getElementById("pulgada").value = pulgada.toFixed(2);
+    document.getElementById("yarda").value = yarda.toFixed(2);
 }
+
 /**
  * Permite convertir grados a radianes y viceversa.
  * @method convertirGR
  * @param {string} id - grados, radianes
  */
-function convertirGR(id){
-    let grad,rad;
+function convertirGR(id) {
+    let grad, rad;
     //TODO: Agregar mensajes de error cuando se agregan letras.
     //TODO: Debería admitir números con coma.
-    if (id=="grados") {
+    if (id == "grados") {
         grad = document.getElementById("grados").value;
         rad = (grad * Math.PI) / 180;
-        document.getElementById("radianes").value=rad;
+        document.getElementById("radianes").value = rad;
+    } else if (id == "radianes") {
+        rad = document.getElementById("radianes").value;
+        grad = (rad * 180) / Math.PI
+        document.getElementById("grados").value = grad;
     }
-        else if (id=="radianes"){
-            rad=document.getElementById("radianes").value;
-            grad=(rad*180)/Math.PI
-        document.getElementById("grados").value=grad;
-        }
 }
 
 /**
@@ -74,49 +70,88 @@ function convertirGR(id){
  * @param mostrar_ocultar
  * @param {string} valor - contiene: val_mostrar, val_ocultar
  */
-let mostrar_ocultar = (valor) =>{
-    if(valor==="val_mostrar"){
-        document.getElementsByName("unDiv")[0].style.display='block';
-    } else if(valor==="val_ocultar"){
-        document.getElementsByName("unDiv")[0].style.display='none';
+let mostrar_ocultar = (valor) => {
+    if (valor === "val_mostrar") {
+        document.getElementsByName("unDiv")[0].style.display = 'block';
+    } else if (valor === "val_ocultar") {
+        document.getElementsByName("unDiv")[0].style.display = 'none';
     }
 }
 /**
  * Permite sumar 2 numeros ingresados por el usuario
  * @param suma
  */
-let suma = () =>{
-    const s1 = Number (document.getElementById("nums1").value);
-    const s2 = Number (document.getElementById("nums2").value);
-    document.getElementById("totalS").value=s1+s2;
+let suma = () => {
+    const s1 = Number(document.getElementById("nums1").value);
+    const s2 = Number(document.getElementById("nums2").value);
+    document.getElementById("totalS").innerHTML = s1 + s2;
 }
 
 /**
  * Permite restar 2 numeros ingresados por el usuario
  * @param resta
  */
-let resta = () =>{
-    const r1 = Number (document.getElementById("numr1").value);
-    const r2 = Number (document.getElementById("numr2").value);
-    document.getElementById("totalR").value=r1-r2;
+let resta = () => {
+    const r1 = Number(document.getElementById("numr1").value);
+    const r2 = Number(document.getElementById("numr2").value);
+    document.getElementById("totalR").innerHTML = r1 - r2;
 }
 
 /**
  * Permite dividir 2 numeros ingresados por el usuario
  * @param divide
  */
-let divide = () =>{
-    const d1 = Number (document.getElementById("numd1").value);
-    const d2 = Number (document.getElementById("numd2").value);
-    document.getElementById("totalD").value=d1/d2;
+let divide = () => {
+    const d1 = Number(document.getElementById("numd1").value);
+    const d2 = Number(document.getElementById("numd2").value);
+    document.getElementById("totalD").innerHTML = d1 / d2;
 }
 
 /**
  * Permite multiplicar 2 numeros ingresados por el usuario
  * @param resta
  */
-let multiplica = () =>{
-    const m1 = Number (document.getElementById("numm1").value);
-    const m2 = Number (document.getElementById("numm2").value);
-    document.getElementById("totalM").value=m1*m2;
+let multiplica = () => {
+    const m1 = Number(document.getElementById("numm1").value);
+    const m2 = Number(document.getElementById("numm2").value);
+    document.getElementById("totalM").innerHTML = m1 * m2;
+}
+
+/**
+ * Permite pasar valores ingresados por el usuario
+ * @param pasarValores
+ */
+function pasarValores() {
+    const distancia = document.getElementById("distancia").value;
+    const unidad = document.getElementById("unidades").value;
+
+    //window.open("segundaWeb.html#"+distancia+"#"+unidad); FORMA QUE NO SE USA MUCHO
+    window.open(`segundaWeb.html#${distancia}#${unidad}`);
+}
+
+/**
+ * Permite tomar los valores ingresados por el usuario
+ * @param tomarValores
+ */
+function tomarValores() {
+    let urlCompleta = window.location.href;
+    urlCompleta = urlCompleta.split("#");
+    const distancia = urlCompleta[1];
+    const unidad = urlCompleta[2];
+    document.getElementById("dist").value = `${distancia} ${unidad}`;
+}
+
+function guardarLS(){
+    const distancia = document.getElementById("distancia").value;
+    const unidad = document.getElementById("unidades").value;
+
+    localStorage.setItem("distanciaLS",distancia);
+    localStorage.setItem("unidadLS",unidad);
+    window.open("2_Web.html");
+}
+
+function cargarLS(){
+    const dist=localStorage.getItem("distanciaLS");
+    const unid=localStorage.getItem("unidadLS");
+    document.getElementById("dist").value = `${dist} ${unid}`;
 }
