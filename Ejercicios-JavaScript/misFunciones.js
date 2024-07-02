27b6744de2356f8cd789eda646f1f9f8a67af0c6
Franciscafalco <franciscafalco4@gmail.com>
2024-07-02 11:40:18 -0300

    Se agregó ejercicio de Animaciones en Canvas

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -286,3 +286,25 @@ let cierreDialog=()=>{
     const dialog=document.getElementById("mensajeError");
     dialog.close();
 }
+
+x=0;
+dx=2;
+function animarAuto(){
+    var canvas=document.getElementById("myCanvas");
+    var ctx= canvas.getContext("2d");
+
+    canvas.width = canvas.width;
+
+    var img=new Image();
+    img.src="images/auto.png";
+
+    img.onload = function (){
+        ctx.drawImage(img, x, 100);
+    }
+
+    if (x>canvas.width){
+        x=0;
+    }
+
+    x+=dx;
+}
